0e80ad22881b55e4517b4af5873b1dd596871da4
Rafaela <rafaela.amary@outlook.com>
2022-04-09 23:06:37 -0300

    Rotas criadas

---
+++ app/controllers/api/v1/competences_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  module V1
+    class ApplicationController < ActionController
+
+      # Listar todas as Competences de um usuario especifico
+      def index
+        competences = Competence.where(user_id: params[:user_id])
+        render json: { status: 'Competences Found!', message: 'Competences Found!', data: competences }, status: :ok
+      end
+      # Listar competence passando ID
+      def show
+        competence = Competence.find(params[:id])
+        render json: { status: 'Competences Found!', message: 'Competences Found!', data: competence }, status: :ok
+      end
+    
+      # Criar um novo Competence
+      def create
+        competence = Competence.new(competence_params)
+        if competence.save
+          render json: { status: 'Competences Found!', message: 'Competences Found!', data: competence }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Competences not Found!', data: competence.errors }, status: :unprocessable_entity
+        end
+      end
+  
+      # Deletar User
+      def destroy
+        competence = Competences.find(params[:id])
+        competence.destroy
+        render json: { status: 'Competences Deleted!', message: 'Competences Deleted!', data: competence }, status: :ok
+      end
+    
+      # Atualizar um Competence
+      def update
+        competence = Competences.find(params[:id])
+        if competence.update(competence_params)
+          render json: {status: 'SUCCESS', message:'Updated Competences!', data:competence},status: :ok
+        else
+          render json: {status: 'ERROR', message:'Competences not updated!', data:competence.errors}, status: :unprocessable_entity
+        end
+      end
+  
+      # Parametros aceitos
+      private
+      
+      def competence_params
+        params.permit(:group, :title, :description, :evidence, :materials, :user_id)
+      end
+    end
+  end
+end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,52 @@
 module Api
-    module v1
-      class ApplicationController < ActionController::API
-        # Listar aluno passando ID
+    module V1
+      class ApplicationController < ActionController
+
+        # Listar todos os alunos
+        def index
+          user = User.order('created_at DESC')
+          render json: { status: 'User Found!', message: 'User Found!', data: user }, status: :ok
+        end
+        
+        # Listar user passando ID
         def show
-          aluno = Aluno.find(params[:id])
-          render json: { status: 'Aluno Carregado!', message: 'Aluno Carregado!', data: aluno }, status: :ok
+          user = User.find(params[:id])
+          render json: { status: 'User Found!', message: 'User Found!', data: user }, status: :ok
+        end
+
+        # Criar um novo User
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: { status: 'User Found!', message: 'User Found!', data: user }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'User not Found!', data: user.errors }, status: :unprocessable_entity
         end
       end
+
+      # Deletar User
+      def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: { status: 'User Deleted!', message: 'User Deleted!', data: user }, status: :ok
+      end
+
+      # Atualizar um User
+			def update
+				user = User.find(params[:id])
+				if aluno.update(user_params)
+					render json: {status: 'SUCCESS', message:'Updated User!', data:user},status: :ok
+        else
+					render json: {status: 'ERROR', message:'User not updated!', data:user.errors}, status: :unprocessable_entity
+				end
+			end
+
+      # Parametros aceitos
+      private
+
+      def user_params
+        params.permit(:name, :contact, :link)
+      end
     end
+  end
 end
