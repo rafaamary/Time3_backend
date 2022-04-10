553928df9a2d4ea42f24440cc603a3310017ebc2
gustavorodr <gustavorodrigues.txt@gmail.com>
2022-04-10 01:19:57 -0300

    nova estrutura de growth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,40 @@ class User < ApplicationRecord
 
   after_create :criar_competences
 
-  def criar_competences 
-    Competence.create(group: 'Fundamentos', title: 'Conhecimento tecnico', description: 'Habitos', evidence: 'link aa', materials: 'link a', user_id: id)
-    Competence.create(group: 'Pessoas', title: 'Organização', description: 'Habitos', evidence: 'link bb', materials: 'link b', user_id: id)
-    Competence.create(group: 'Testes', title: 'Tipos de testes', description: 'Habitos', evidence: 'link cc',  materials: 'link c', user_id: id)
-  end
+  def criar_competences
+    Competence.create(group: 'Fundamentos',
+                      title: 'Tem noção clara de como utilizar o terminal',
+                      description: "Entende os conceitos de arquivos, diretórios e path do terminal \n
+                        Utilizar os comandos básicos do unix (cd, ls, mkdir, rm, cp, cat less, mv, grep, tar, alias) \n
+                        Entende quando utilizar sudo e sabe o básico permissões de arquivos e usuários (chmod) \n
+                        Entende quais são os arquivos de configurações do shell (bashrc, profile e /env/)",
+                      evidence: '',
+                      materials: '',
+                      user_id: id)
+    Competence.create(group: 'Melhores Praticas',
+                      title: 'Sabe identificar quando há a necessidade de documentar suas entregas',
+                      description: "Documentar um novo script \n
+                      Documentar um trecho de um script que esteja complexo \n
+                      Documentar scripts existentes que não possuem documentação",
+                      evidence: '',
+                      materials: '',
+                      user_id: id)
+      Competence.create(group: 'Pessoas',
+                      title: 'Desenvolve sua autonomia para fazer suas atividades por conta própria',
+                      description: "Acaba com todas as possibilidades de pesquisas antes de pedir ajuda \n
+                      Não tem medo de arriscar a fazer sozinho, porém com cautela, para aprender sozinho \n
+                      Pede sugestões e opiniões de atividades feitas para aprimorar seus feitos",
+                      evidence: '',
+                      materials: '',
+                      user_id: id)
+      Competence.create(group: 'Programação',
+                      title: 'Entende os conceitos básicos de uma API',
+                      description: "O que é uma API? Diferença entre REST, RESTfull e SOAP \n
+                      Autenticação + segurança: Basic Auth, OAuth, JWT \n
+                      HTTP Status Code \n
+                      Tráfego do dado obtido do banco de dados",
+                      evidence: '',
+                      materials: '',
+                      user_id: id)
+    end
 end
