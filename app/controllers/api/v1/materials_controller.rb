3c9575309826b8265fa19e24a27af22c6085be6b
gustavorodr <gustavorodrigues.txt@gmail.com>
2022-04-11 11:01:33 -0300

    novo modelo de growth

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class MaterialsController < ActionController::API
       def show
-        references = Competence.where.not(materials: nil).joins(:user).where(title: params[:title]).pluck(:name, :materials)
+        #references = Competence.where.not(materials: nil).joins(:user).where(title: params[:title]).pluck(:name, :materials)
+        references = Competence.where.where(title: params[:title]).pluck(:materials).compact
         render json: { status: 'Competences Found!', message: 'Competences Found!', data: references  }, status: :ok
       end
       # Parametros aceitos
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,37 +6,39 @@ class User < ApplicationRecord
   def criar_competences
     Competence.create(group: 'Fundamentos',
                       title: 'Tem noção clara de como utilizar o terminal',
-                      description: "Entende os conceitos de arquivos, diretórios e path do terminal \n
-                        Utilizar os comandos básicos do unix (cd, ls, mkdir, rm, cp, cat less, mv, grep, tar, alias) \n
-                        Entende quando utilizar sudo e sabe o básico permissões de arquivos e usuários (chmod) \n
-                        Entende quais são os arquivos de configurações do shell (bashrc, profile e /env/)",
-                      evidence: nil,
-                      materials: nil,
+                      description: "Entende os conceitos de arquivos, diretórios e path do terminal",
+                      evidence: "Fiz tal coisa e aprendi isso",
+                      materials: "www.link.com",
                       user_id: id)
-    Competence.create(group: 'Melhores Praticas',
-                      title: 'Sabe identificar quando há a necessidade de documentar suas entregas',
-                      description: "Documentar um novo script \n
-                      Documentar um trecho de um script que esteja complexo \n
-                      Documentar scripts existentes que não possuem documentação",
-                      evidence: nil,
-                      materials: nil,
+    Competence.create(group: 'Fundamentos',
+                      title: 'Tem noção clara de comandos Git e do Github',
+                      description: "Sabe os comandos básicos de Git para desenvolver um projeto(pull, push, create branch, commit, merge);",
+                      evidence: "Fiz fiz etc e coisa e aprendi aquilo lá",
+                      materials: "www.git.com",
                       user_id: id)
-      Competence.create(group: 'Pessoas',
+    Competence.create(group: 'Pessoas',
                       title: 'Desenvolve sua autonomia para fazer suas atividades por conta própria',
-                      description: "Acaba com todas as possibilidades de pesquisas antes de pedir ajuda \n
-                      Não tem medo de arriscar a fazer sozinho, porém com cautela, para aprender sozinho \n
-                      Pede sugestões e opiniões de atividades feitas para aprimorar seus feitos",
-                      evidence: nil,
-                      materials: nil,
+                      description: "Acaba com todas as possibilidades de pesquisas antes de pedir ajuda",
+                      evidence: "Fiz fiz etc e coisa e aprendi aquilo lá",
+                      materials: "www.git.com",
                       user_id: id)
-      Competence.create(group: 'Programação',
+    Competence.create(group: 'Pessoas',
+                      title: 'Aceita bem feedbacks construtivos e entende que é para desenvolvimento de sua carreira/vida',
+                      description: "Não fica mal por muito tempo quando recebe um feedback negativo",
+                      evidence: "Aprendi a ser assim e lidei com tal coisa",
+                      materials: "Consegui lidar assim e tal e meu lider noe.sobrenome me avaliou",
+                      user_id: id)
+    Competence.create(group: 'Programação',
                       title: 'Entende os conceitos básicos de uma API',
-                      description: "O que é uma API? Diferença entre REST, RESTfull e SOAP \n
-                      Autenticação + segurança: Basic Auth, OAuth, JWT \n
-                      HTTP Status Code \n
-                      Tráfego do dado obtido do banco de dados",
-                      evidence: nil,
-                      materials: nil,
+                      description: "O que é uma API? Diferença entre REST, RESTfull e SOAP",
+                        evidence: "www.github.com",
+                        materials: "material.com",
                       user_id: id)
-    end
+    Competence.create(group: 'Programação',
+                      title: 'Entende os conceitos básicos de desenvolvimento de Banco de dados',
+                      description: "Saber para que serve um banco de dados e quando utilizar",
+                        evidence: "www.gitlab.com",
+                        materials: "artigo.com",
+                      user_id: id)
+  end
 end
